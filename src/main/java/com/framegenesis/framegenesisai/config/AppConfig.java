a01a2385acec8b1c993796ccb29ca8269f01c69c
Ubaid Rehman <urkhan556@gmail.com>
2026-05-23 12:02:27 +0530

    updated some code

--- src/main/java/com/framegenesis/framegenesisai/config/AppConfig.java
+++ src/main/java/com/framegenesis/framegenesisai/config/AppConfig.java
@@ -2,9 +2,11 @@ package com.framegenesis.framegenesisai.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
+@EnableAsync
 public class AppConfig {
 
     @Bean
